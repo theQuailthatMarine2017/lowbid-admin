85f5a31b873de7cdd6813fb1b55f46e7dd29bd2f
kingphisher <kingphisher@RunwhileYouCn.local>
2022-06-14 18:28:45 +0300

    oirigns

--- admin.js
+++ admin.js
@@ -22,7 +22,7 @@ if (cluster.isMaster) {
     const app = express();
     const port = 4000;
 
-    var origins = ['*','https://lowbids.co.ke','lowbids.co.ke','https://dashboard.lowbids.co.ke','dashboard.lowbids.co.ke','http://localhost:8081'];
+    var origins = ['*','https://lowbids.co.ke','lowbids.co.ke','https://dashboard.lowbids.co.ke','dashboard.lowbids.co.ke','http://localhost:8081','https://winportal.lowbids.co.ke','winportal.lowbids.co.ke'];
 
     app.use(cors({
       origin: origins
